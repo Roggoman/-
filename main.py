e73d66ac3fa0334e4e955a66c917b1194aaa7e23
Egor Yakovlev <egorsambo0405@mail.ru>
2024-03-15 17:42:45 +0300

    1

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, url_for, render_template
+from flask import request
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html', title='Домашняя страница')
+
+@app.route('/training/<prof>')
+def train(prof):
+    flag = True if prof in ["инженер", "строитель"] else False
+    return render_template('training.html', title=prof, flag=flag)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
